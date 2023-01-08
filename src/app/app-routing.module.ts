99076f6f896a4a8108288f09ded4b4d0653c814a
Souguir Oumaima <81293137+Souguiroumaima7@users.noreply.github.com>
2023-01-08 15:37:57 +0100

    committing in angular project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './Components/about/about.component';
+import { BacktotopComponent } from './Components/backtotop/backtotop.component';
+import { CopyrightComponent } from './Components/copyright/copyright.component';
+import { ExpertiseComponent } from './Components/expertise/expertise.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { HomeComponent } from './Components/home/home.component';
+import { MapComponent } from './Components/map/map.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { ServiceComponent } from './Components/service/service.component';
+import { SpinnerComponent } from './Components/spinner/spinner.component';
+import { TeamComponent } from './Components/team/team.component';
+import { TestimonialComponent } from './Components/testimonial/testimonial.component';
+import { VideoModalComponent } from './Components/video-modal/video-modal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path : "" , component:HomeComponent},
+{path : "about" , component:AboutComponent},
+{path : "Contact" , component:AboutComponent},
+{path : "Backtotop" , component:BacktotopComponent},
+{path :"copyright", component:CopyrightComponent},
+{path :"expertise", component:ExpertiseComponent},
+{path :"header", component:HeaderComponent},
+{path :"map", component:MapComponent},
+{path :"navbar", component:NavbarComponent},
+{path :"projects", component:ProjectsComponent},
+{path :"service", component:ServiceComponent},
+{path :"spinner", component:SpinnerComponent},
+{path :"team", component:TeamComponent},
+{path :"testimonial", component:TestimonialComponent},
+{path :"videomodal", component:VideoModalComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { SpinnerComponent } from './Components/spinner/spinner.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { VideoModalComponent } from './Components/video-modal/video-modal.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ExpertiseComponent } from './Components/expertise/expertise.component';
+import { ServiceComponent } from './Components/service/service.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { TeamComponent } from './Components/team/team.component';
+import { TestimonialComponent } from './Components/testimonial/testimonial.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { MapComponent } from './Components/map/map.component';
+import { CopyrightComponent } from './Components/copyright/copyright.component';
+import { BacktotopComponent } from './Components/backtotop/backtotop.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    SpinnerComponent,
+    HeaderComponent,
+    VideoModalComponent,
+    AboutComponent,
+    ExpertiseComponent,
+    ServiceComponent,
+    ProjectsComponent,
+    TeamComponent,
+    TestimonialComponent,
+    ContactComponent,
+    MapComponent,
+    CopyrightComponent,
+    BacktotopComponent
   ],
   imports: [
     BrowserModule,
